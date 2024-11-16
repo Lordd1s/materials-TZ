190b79ae8a9abf74493dcfc8bb5a69151d08bb64
Dias <101506957+Lordd1s@users.noreply.github.com>
2024-11-16 16:28:31 +0500

    Update utils.py

--- app/materials/api/v1/services/utils.py
+++ app/materials/api/v1/services/utils.py
@@ -1,7 +1,7 @@
 from materials.models import Material
 
 
-def calculate_total(category):
+def calculate_total(category) -> int:
     materials = Material.objects.filter(category=category).select_related('category')
     total = sum(material.price for material in materials)
     for child in category.children.all():
